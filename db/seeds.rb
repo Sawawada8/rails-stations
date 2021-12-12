81e2f5f3712c6b01f3b7e9be4906416a8d1ce7d9
sawada <sawada@csplan.jp>
2021-12-12 20:43:23 +0900

    station02

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,28 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  movies = Movie.create([
+    {
+      name: 'Star Wars',
+      year: '2021',
+      description: 'description text',
+      image_url: 'https://image.example',
+      is_showing: 1,
+    },
+    {
+      name: 'Star Wars2',
+      year: '2021',
+      description: 'description text',
+      image_url: 'https://image.example',
+      is_showing: 1,
+    },
+    {
+      name: 'Star Wars3',
+      year: '2021',
+      description: 'description text',
+      image_url: 'https://image.example',
+      is_showing: 1,
+    },
+  ])
+    # { name: 'Lord of the Rings' }])
+  # Character.create(name: 'Luke', movie: movies.first)
